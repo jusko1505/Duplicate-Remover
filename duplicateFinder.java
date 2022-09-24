073b360490fa856dcfc3280dae5b51d82ddebdb1
jusko1505 <97807951+jusko1505@users.noreply.github.com>
2022-09-23 20:54:59 -0700

    changed from MD5 to CRC32, Public methods -> Private

--- duplicateFinder.java
+++ duplicateFinder.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,21 +10,27 @@ import java.security.NoSuchAlgorithmException;
 import java.util.*;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.zip.CRC32;
 
 public class duplicateFinder {
-
-    static HashMap<String, File> map;
+    /*private HashMap<Long, File> map;*/
+    private static HashMap<Long, File> map;
     public static void main(String[] args) throws IOException, NoSuchAlgorithmException{
+
+        duplicateFinder df = new duplicateFinder();
         
         Scanner sc = new Scanner(System.in);
+        /*
         MessageDigest md = MessageDigest.getInstance("MD5");
-        welcomeMessage();
+        */
+        df.welcomeMessage();
 
         String path = sc.nextLine();
         long t2 = (new Date()).getTime();
         File directory = new File(path);
         File [] directoryList = directory.listFiles();
-        File dupeFolderPath = createDupeFolder(path);
+        File dupeFolderPath = df.createDupeFolder(path);
+        int count =0;
 
         /* If the directory list is a folder */
         if (directoryList != null){
@@ -31,15 +38,17 @@ public class duplicateFinder {
             if(directoryList.length != 0){
                 /* Initialize hashmap capacity because resizing is expensive  */
                 int hsSize = (int)((directoryList.length+1)/0.75);
-                map = new HashMap<String, File>(hsSize);
+                map = new HashMap<Long, File>(hsSize);
                 for (File potentialDupe: directoryList){
                     /* For each file, if it is not a folder, find its checksum */
                     if (!potentialDupe.isDirectory()){
-                        String hash = checksum(md, potentialDupe);
+                        System.out.print("|"+count++);
+                        /*String hash = df.MD5checksum(md, potentialDupe);*/
+                        long hash = df.CRC32Checksum(potentialDupe);
                         /* If our hashmap contains that file, move it to the duplicates folder */
                         if (map.containsKey(hash)){
-                            if(compareFileSize(potentialDupe, map.get(hash))){
-                                cutNPasteFile(potentialDupe, dupeFolderPath);
+                            if(df.compareFileSize(potentialDupe, map.get(hash))){
+                                df.cutNPasteFile(potentialDupe, dupeFolderPath);
                             }
                         }
                         else{
@@ -65,7 +74,57 @@ public class duplicateFinder {
         long executionTime = (t3 - t2);
         System.out.println("Total execution time: " + executionTime +" ms");
     }
-    private static String checksum(MessageDigest digest, File file) throws IOException{
+    /* creates the duplicates folder */
+    private File createDupeFolder(String path){
+        String temp = path +"\\Duplicates";
+        try{
+            Path newFolder = Paths.get(temp);
+            Files.createDirectories(newFolder);
+        }
+        catch(IOException e){
+            System.err.println("Duplicates folder cannot be created! " + e.getMessage());
+        }
+        File folderPath = new File(temp);
+        return folderPath;
+
+    }
+    /*moves the file to the duplicates folder*/
+    private void cutNPasteFile(File file, File destination){
+        File appendToDestination = new File(destination.getAbsolutePath() + "\\" + file.getName());
+        file.renameTo(appendToDestination);
+    }
+    /* returns true if the two files have the same size
+    used in combination with checksum to find if two files are the same */
+    private boolean compareFileSize(File file1, File file2){
+        double bytes1 = file1.length();
+        double bytes2 = file2.length();
+        if (bytes1 == bytes2){
+            return true;
+        }
+        return false;
+    }
+
+    private long CRC32Checksum(File file) throws IOException{
+        FileInputStream fis = new FileInputStream(file);
+        CRC32 crc = new CRC32();
+        byte[] byteArray = new byte[32];
+        int bytesCount =  0;
+
+        while ((bytesCount = fis.read(byteArray)) != -1){
+            crc.update(byteArray, 0, bytesCount);
+        }
+        fis.close();
+
+        return crc.getValue();
+    }
+
+    private void welcomeMessage(){
+        System.out.println("Please make sure that you do not have a folder called \"Duplicates\"");
+        System.out.println("Enter the directory in which you want to find duplicates:");
+    }
+
+    /* 
+    private String MD5checksum(MessageDigest digest, File file) throws IOException{
         //reads bytes from a file
         FileInputStream fis = new FileInputStream(file);
         //byte array reads data in chunks
@@ -87,38 +146,5 @@ public class duplicateFinder {
 
         return sb.toString();
     }
-    /* creates the duplicates folder */
-    public static File createDupeFolder(String path){
-        String temp = path +"\\Duplicates";
-        try{
-            Path newFolder = Paths.get(temp);
-            Files.createDirectories(newFolder);
-        }
-        catch(IOException e){
-            System.err.println("Duplicates folder cannot be created! " + e.getMessage());
-        }
-        File folderPath = new File(temp);
-        return folderPath;
-
-    }
-    /*moves the file to the duplicates folder*/
-    public static void cutNPasteFile(File file, File destination){
-        File appendToDestination = new File(destination.getAbsolutePath() + "\\" + file.getName());
-        file.renameTo(appendToDestination);
-    }
-    /* returns true if the two files have the same size
-    used in combination with checksum to find if two files are the same */
-    public static boolean compareFileSize(File file1, File file2){
-        double bytes1 = file1.length();
-        double bytes2 = file2.length();
-        if (bytes1 == bytes2){
-            return true;
-        }
-        return false;
-    }
-
-    public static void welcomeMessage(){
-        System.out.println("Please make sure that you do not have a folder called \"Duplicates\"");
-        System.out.println("Enter the directory in which you want to find duplicates:");
-    }
+    */
 }
